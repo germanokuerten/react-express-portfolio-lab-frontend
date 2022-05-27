444eadca5d572e03fa8f4120cdcec6630c38098c
Germano Kuerten <germanokuerten@gmail.com>
2022-05-27 16:23:02 -0700

    app.js setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
+// Import Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+// Import the Route and Switch component from Router
+import { Route, Routes } from "react-router-dom"
+
+// Import Pages    
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+
 function App() {
+
+  // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
+  const URL = "https://gk-react-express-portfolio.herokuapp.com/"
+
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      {/* // Set up routes */}
+
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects URL={URL}/>} />
+          <Route path="/about" element={<About URL={URL}/>} />
+        </Routes>
+      <Footer />
+
     </div>
   );
 }
